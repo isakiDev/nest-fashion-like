6979e0480d9da6d0c80838440cdff9399547fd68
isakiDev <gaspar.c.developer@gmail.com>
2024-04-03 01:45:56 -0300

    update: load host from .env

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@nestjs/common'
 import { MailerService } from '@nestjs-modules/mailer'
 
 import { type SendUserConfirmationDto } from './dtos'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class EmailService {
   constructor (
-    private readonly mailerService: MailerService
+    private readonly mailerService: MailerService,
+    private readonly configService: ConfigService
   ) {}
 
   async sendUserConfirmation (sendUserConfirmationDto: SendUserConfirmationDto) {
     const { name, email, token } = sendUserConfirmationDto
 
-    const url = `http://localhost:5173/auth/confirm?token=${token}`
+    const url = `${this.configService.get('FRONT_END_HOST')}/auth/confirm?token=${token}`
     const subject = `Welcome ${name}`
 
     await this.mailerService.sendMail({
