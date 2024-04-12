4858600c109a6de8034e676f49913b986d525ae0
isakiDev <gaspar.c.developer@gmail.com>
2024-04-11 20:13:48 -0400

    add ssl to postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,12 @@ import { CloudinaryModule } from './cloudinary/cloudinary.module'
     }),
 
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE !== 'prod'
+          ? { rejectUnauthorized: false }
+          : null
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
