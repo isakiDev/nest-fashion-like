d1c0f4f09b6343961e8e918e4363b53526b860c3
isakidev <gaspar.c.developer@gmail.com>
2024-08-03 17:48:39 -0400

    chore: remove old dist

--- dist/auth/guards/user-role.guard.d.ts
+++
@@ -1,8 +0,0 @@
-import { type CanActivate, type ExecutionContext } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { type Observable } from 'rxjs';
-export declare class UserRoleGuard implements CanActivate {
-    private readonly reflector;
-    constructor(reflector: Reflector);
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean>;
-}
--- dist/reaction/dtos/create-interaction.dto.d.ts
+++
@@ -1,4 +0,0 @@
-import { TypeReaction } from '../interfaces/reaction.interface';
-export declare class CreateReactionDto {
-    readonly type: TypeReaction;
-}
